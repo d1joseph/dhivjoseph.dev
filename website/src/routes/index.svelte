42768521e58495e13c761a4c14b161778ba1394f
d1joseph <dhiv.joseph@gmail.com>
2022-07-25 23:08:47 +1000

    feat():basic html markup, library and params folder.

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<svelte:head>
+    <title>Welcome to dhivjoseph.dev 👋</title>
+</svelte:head>
+
+<h1>Dhiv Joseph</h1>
+<h2>Software Engineer & Computer Scientist</h2>
+<h3>Welcome travellers of the internet. You have arrived.</h3>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
